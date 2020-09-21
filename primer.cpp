f54df70f3cbc3f8879ac9d7f071b8d495a567412
Yury Vasev <vasevyp@mail.ru>
2020-09-21 20:06:59 +0300

    first commit

---
+++ primer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\t\t     /\\" << endl;
+    cout << "\t\t    /  \\" << endl;
+    cout << "\t\t   /    \\" << endl;
+    cout << "\t\t  /______\\" << endl;
+    cout << "\t\t / |    | \\" << endl;
+    cout << "\t\t   |    |" << endl;
+    cout << "\t\t   |    |" << endl;
+    cout << "\t\t   |    |" << endl;
+    cout << "\t\t   |____|" << endl;
+    return 0;
+}
